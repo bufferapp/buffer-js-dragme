56e418add9a0d9c64006c67fcd1eb83bac9d381c
Philippe Masset <pioul06+github@gmail.com>
2017-02-22 22:03:46 +0100

    Transform to es6 class

--- src/dragme.js
+++ src/dragme.js
@@ -7,70 +7,67 @@
  *        on which to prevent dragging
  */
 
-const DragMe = function(el, options) {
-  const defaults = {
-    cancel: null,
-  };
+class DragMe {
+  constructor(el, options) {
+    const defaults = {
+      cancel: null,
+    };
 
-  options = options || {};
+    options = options || {};
 
-  this.options = Object.assign(defaults, options);
+    this.options = Object.assign(defaults, options);
 
-  this.el = el;
-  this.body = document.body;
-  this.onMousedown = this.onMousedown.bind(this);
-  this.onMove = this.onMove.bind(this);
-  this.release = this.release.bind(this);
+    this.el = el;
+    this.body = document.body;
 
-  this.el.classList.add('ui-draggable');
-  this.el.addEventListener('mousedown', this.onMousedown);
+    this.el.classList.add('ui-draggable');
+    this.el.addEventListener('mousedown', this.onMousedown);
 
-  ['mozTransform', 'msTransform', 'oTransform', 'webkitTransform', 'transform'].forEach((prop) => {
-    if (prop in document.documentElement.style) {
-      this.transform = prop;
-    }
-  }, this);
-};
-
-DragMe.prototype.onMousedown = function(e) {
-  if (this.options.cancel && this.shouldCancel(e.target)) {
-    return;
+    ['mozTransform', 'msTransform', 'oTransform', 'webkitTransform', 'transform'].forEach((prop) => {
+      if (prop in document.documentElement.style) {
+        this.transform = prop;
+      }
+    });
   }
 
-  const style = window.getComputedStyle(this.el)[this.transform];
-  const coords = style && style.match(/-*\d+/g);
+  shouldCancel(target) {
+    return target.closest(this.options.cancel);
+  }
 
-  this.origX = coords ? parseInt(coords[4], 10) : 0;
-  this.origY = coords ? parseInt(coords[5], 10) : 0;
+  onMousedown = (e) => {
+    if (this.options.cancel && this.shouldCancel(e.target)) {
+      return;
+    }
 
-  this.dragStartX = e.pageX;
-  this.dragStartY = e.pageY;
+    const style = window.getComputedStyle(this.el)[this.transform];
+    const coords = style && style.match(/-*\d+/g);
 
-  this.body.addEventListener('mousemove', this.onMove);
-  this.body.addEventListener('mouseup', this.release);
-  this.body.addEventListener('mouseleave', this.release);
-};
+    this.origX = coords ? parseInt(coords[4], 10) : 0;
+    this.origY = coords ? parseInt(coords[5], 10) : 0;
 
+    this.dragStartX = e.pageX;
+    this.dragStartY = e.pageY;
 
-DragMe.prototype.onMove = function(e) {
-  const x = this.origX - this.dragStartX + e.pageX;
-  const y = this.origY - this.dragStartY + e.pageY;
+    this.body.addEventListener('mousemove', this.onMove);
+    this.body.addEventListener('mouseup', this.release);
+    this.body.addEventListener('mouseleave', this.release);
+  };
 
-  this.el.classList.add('ui-dragging');
-  this.el.style[this.transform] = `translate(${x}px, ${y}px)`;
-};
+  onMove = (e) => {
+    const x = this.origX - this.dragStartX + e.pageX;
+    const y = this.origY - this.dragStartY + e.pageY;
 
+    this.el.classList.add('ui-dragging');
+    this.el.style[this.transform] = `translate(${x}px, ${y}px)`;
+  };
 
-DragMe.prototype.release = function() {
-  this.el.classList.remove('ui-dragging');
-  this.body.removeEventListener('mousemove', this.onMove);
-  this.body.removeEventListener('mouseup', this.release);
-  this.body.removeEventListener('mouseleave', this.release);
-};
+  release = () => {
+    this.el.classList.remove('ui-dragging');
 
-
-DragMe.prototype.shouldCancel = function(target) {
-  return target.closest(this.options.cancel);
-};
+    this.body.removeEventListener('mousemove', this.onMove);
+    this.body.removeEventListener('mouseup', this.release);
+    this.body.removeEventListener('mouseleave', this.release);
+  };
+}
 
 export default DragMe;
